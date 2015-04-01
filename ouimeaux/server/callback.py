c81f70dd25f3993047dc45668510a4d18f2cec0a
jason.mckee <jason.mckee@servicenow.com>
2015-04-01 10:49:44 -0700

    added callback to REST web service

---
+++ ouimeaux/server/callback.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+class WebCallback:
+    def __init__(self,device,url,user,password):
+        self.device = device
+        self.url = url
+        self.user = user
+        self.password = password
+        print 'registered callback to: ', self.url
+        
+    def doCallback(self,state):
+        print 'setting ', self.device.serialnumber,' state to ',state
+        # build payload
+        data = {
+            'device_state': state,
+            'device_serial_number': self.device.serialnumber
+        }
+        
+        # Set proper headers
+        headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+        # Do the HTTP request
+        response = requests.post(self.url, auth=(self.user, self.password), headers=headers ,data=json.dumps(data))
+
+        # Check for HTTP codes other than 200
+        if response.status_code != 200: 
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+
+            # Decode the JSON response into a dictionary and use the data
+            data = response.json()
+            print(data)
+
+    @property
+    def url(self):
+        return self.url
+    @property
+    def user(self):
+        return self.user
